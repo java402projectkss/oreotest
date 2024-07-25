4d1e632ff5a9f3c74dd235c97546dc5bdf8648cf
5ore5o <5ore5o@naver.com>
2024-07-25 15:27:21 +0900

    test

---
+++ shopping/src/shopping/test.java
@@ -0,0 +1,13 @@
+package shopping;
+
+public class test {
+
+	public static void main(String[] args) {
+		String mid="ȫ�浿";
+		System.out.println(mid);
+		int b=10;
+		int c=20;
+		String user="hong";
+	}
+
+}
